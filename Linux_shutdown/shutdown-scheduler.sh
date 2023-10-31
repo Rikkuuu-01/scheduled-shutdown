b279b968b121a2b1d64dc49b83d922d3b48fadfd
Rikkuuu-01 <129836270+Rikkuuu-01@users.noreply.github.com>
2023-11-01 01:48:46 +0800

    Update shutdown-scheduler.sh

--- Linux_shutdown/shutdown-scheduler.sh
+++ Linux_shutdown/shutdown-scheduler.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Prompt the user for an extension time
-read -p "Do you want to extend the shutdown time by [minutes]? (Type 0 to cancel): " extend
-
-# Check if the extension time is at least 10 minutes
-if [ "$extend" -lt 10 ]; then
-    echo "The minimum extension time is 10 minutes. Please enter a valid time."
-    exit 1
-fi
-
-# Schedule the shutdown with the extension time
-echo "shutdown -h +$extend" | at now + 1 minute
+while true; do
+    read -p "Enter the number of minutes until shutdown (0 to cancel, 'exit' to exit): " time
+    if [[ "$time" == "exit" ]]; then
+        break
+    elif ! [[ "$time" =~ ^[0-9]+$ ]]; then
+        echo "Invalid input. Please enter a valid time or 'exit' to cancel."
+    elif [[ "$time" -lt 10 ]]; then
+        echo "The minimum extension time is 10 minutes. Please enter a valid time."
+    else
+        sudo shutdown -h +$time
+        break
+    fi
+done
